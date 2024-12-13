8a6184c5d2b91ef9ed85ee04d46b09c5dc6435b8
aymanelfadl <aymanelfadl16@gmail.com>
2024-12-13 16:53:26 +0100

    hey

--- getNextLine/get_next_line.c
+++ getNextLine/get_next_line.c
@@ -1,76 +1,102 @@
-
 #include "get_next_line.h"
 
-char	*ft_strjoin(char const *s1, char const *s2)
+char *ft_strjoin(char const *s1, char const *s2)
 {
-	char	*res;
-	size_t	lens1;
-	size_t	lens2;
+    char *res;
+    size_t lens1;
+    size_t lens2;
 
-	lens1 = 0;
-	lens2 = 0;
-	if (!s1 && !s2)
-		return (NULL);
-	if (s1)
-		lens1 += strlen(s1);
-	if (s2)
-		lens2 += strlen(s2);
-	res = (char *)calloc((lens1 + lens2 + 1), sizeof(char));
-	if (!res)
-		return (NULL);
-	memcpy(res, s1, lens1);
-	memcpy(res + lens1, s2, lens2);
-	return (res);
+    if (s1 == NULL)
+        lens1 = 0;
+    else
+        lens1 = strlen(s1);
+
+    if (s2 == NULL)
+        lens2 = 0;
+    else
+        lens2 = strlen(s2);
+
+    if (!s1 && !s2)
+        return NULL;
+
+    res = malloc(lens1 + lens2 + 1);
+    if (!res)
+        return NULL;
+
+    if (s1)
+        memcpy(res, s1, lens1);
+    if (s2)
+        memcpy(res + lens1, s2, lens2);
+    res[lens1 + lens2] = '\0';
+
+    return res;
 }
 
 char *get_line(char *buff)
 {
-    char *res;
-    int len;
-
-    len = 0;
+    int len = 0;
     while (buff[len] && buff[len] != '\n')
         len++;
-    if (buff[len] == '\0')
-        res = malloc(len + 1);
-    else
-        res = malloc(len + 2);
-    if(!res)
-    {
-        free(buff);
+    if (buff[len] == '\n')
+        len++;
+    char *res = malloc(len + 1);
+    if (!res)
         return NULL;
-    }
-    memcpy(res, buff,len);
+    memcpy(res, buff, len);
     res[len] = '\0';
     return res;
 }
 
 char *get_next_line(int fd)
 {
-    char *buffer;
-    char *tmpbuf;
-    static char *holder;
+    static char *remainder = NULL;
+    char *buffer = NULL;
+    char *line = NULL;
+    char *temp_buffer = NULL;
+    ssize_t bytes_read;
 
-    buffer = NULL;
-    tmpbuf = malloc(BUFFER_SIZE);
-    if (!tmpbuf)
-        free(tmpbuf);
-    if (read(fd, tmpbuf, BUFFER_SIZE) > -1)
+    if (fd < 0 || BUFFER_SIZE <= 0)
+        return NULL;
+
+    temp_buffer = malloc(BUFFER_SIZE + 1);
+    if (!temp_buffer)
+        return NULL;
+    if (remainder)
+        buffer = strdup(remainder);
+    else
+        buffer = strdup("");
+    free(remainder);
+    remainder = NULL;
+    while (1)
     {
-            buffer = ft_strjoin(buffer, holder);
-            buffer = ft_strjoin(buffer, tmpbuf);
-            while (!strchr(buffer,'\n'))
-            {
-                int i = read(fd, tmpbuf, BUFFER_SIZE);
-                buffer = ft_strjoin(buffer, tmpbuf);
-                if (i == 0)
-                    break;
-            }
-            holder = strchr(buffer,'\n') + 1;
-            return get_line(buffer);
+        bytes_read = read(fd, temp_buffer, BUFFER_SIZE);
+        if (bytes_read < 0)
+        {
+            free(temp_buffer);
+            free(buffer);
+            return NULL;
+        }
+        temp_buffer[bytes_read] = '\0';
+
+        char *new_buffer = ft_strjoin(buffer, temp_buffer);
+        free(buffer);
+        buffer = new_buffer;
+
+        if (strchr(buffer, '\n') || bytes_read == 0)
+            break;
     }
-    return NULL;
-    
+    free(temp_buffer);
+    if (!*buffer)
+    {
+        free(buffer);
+        return NULL;
+    }
+    line = get_line(buffer);
+    char *newline = strchr(buffer, '\n');
+    if (newline)
+        remainder = strdup(newline + 1);
+    free(buffer);
+    return line;
 }
 
 int main()
@@ -78,11 +104,14 @@ int main()
     int fd = open("file.txt", O_RDONLY);
     if (fd < 0)
         return 0;
-    char *s = NULL;
-    while ((s = get_next_line(fd)))
+
+    char *s;
+    while ((s = get_next_line(fd)) != NULL)
     {
-        printf("%s\n", s);
-        free(s);  // Don't forget to free the memory for each line
+        printf("%s", s);
+        free(s);
     }
 
+    close(fd);
+    return 0;
 }
--- getNextLine/get_next_line.h
+++ getNextLine/get_next_line.h
@@ -8,7 +8,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define BUFFER_SIZE 100
+#define BUFFER_SIZE 10000000
 
 char *get_next_line(int fd);
 
