03f8c7fa86d4a958669764ec453cb16f811d3232
aymanelfadl <aymanelfadl16@gmail.com>
2024-12-14 18:06:47 +0100

    :)

--- getNextLine/get_next_line.c
+++ getNextLine/get_next_line.c
@@ -1,69 +1,84 @@
 #include "get_next_line.h"
 
-char *get_line(char *buff)
+char	*get_line(char *buff)
 {
-    int len = 0;
-    while (buff[len] && buff[len] != '\n')
-        len++;
-    if (buff[len] == '\n')
-        len++;
-    char *res = malloc(len + 1);
-    if (!res)
-        return NULL;
-    ft_memcpy(res, buff, len);
-    res[len] = '\0';
-    return res;
-}
-char *ft_creatbuffer(int fd, char *buffer)
-{
-    size_t bytes_read;
-    char *temp_buffer;
-    char *new_buffer;
+	int		len;
+	char	*res;
 
-    temp_buffer = malloc(BUFFER_SIZE + 1);
-    if (!temp_buffer)
-        return NULL;
-    while (1)
-    {
-        bytes_read = read(fd, temp_buffer, BUFFER_SIZE);
-        if (bytes_read < 0)
-        {
-            free(temp_buffer);
-            free(buffer);
-            return NULL;
-        }
-        temp_buffer[bytes_read] = '\0';
-        new_buffer = ft_strjoin(buffer, temp_buffer);
-        free(buffer);
-        buffer = new_buffer;
-        if (ft_strchr(buffer, '\n') || bytes_read == 0)
-            break;
-    }
-    free(temp_buffer);
-    return buffer;
+	len = 0;
+	while (buff[len] && buff[len] != '\n')
+		len++;
+	if (buff[len] == '\n')
+		len++;
+	res = malloc(len + 1);
+	if (!res)
+		return (NULL);
+	ft_memcpy(res, buff, len);
+	res[len] = '\0';
+	return (res);
+}
+char	*ft_update_holder(char **holder)
+{
+	char	*temp;
+
+	if (*holder)
+	{
+		temp = ft_strdup(*holder);
+		free(*holder);
+		*holder = NULL;
+	}
+	else
+		temp = ft_strdup("");
+	return (temp);
 }
 
-char *get_next_line(int fd)
+char	*ft_creatbuffer(int fd, char *buffer)
 {
-    static char *holder = NULL;
-    char *line = NULL;
-    char *buffer = NULL;
+	size_t	bytes_read;
+	char	*temp_buffer;
+	char	*new_buffer;
 
-    if (fd < 0 || BUFFER_SIZE <= 0)
-        return NULL;
-    if (holder)
-        buffer = ft_creatbuffer(fd, ft_strdup(holder));
-    else
-        buffer = ft_creatbuffer(fd, ft_strdup(""));
-    free(holder);
-    if (!buffer || !*buffer)
-    {
-        free(buffer);
-        return NULL;
-    }
-    line = get_line(buffer);
-    if (ft_strchr(buffer, '\n'))
-        holder = ft_strdup(ft_strchr(buffer, '\n') + 1);
-    free(buffer);
-    return line;
+	temp_buffer = malloc(BUFFER_SIZE + 1);
+	if (!temp_buffer)
+		return (NULL);
+	while (1)
+	{
+		bytes_read = read(fd, temp_buffer, BUFFER_SIZE);
+		if (bytes_read < 0)
+		{
+			free(buffer);
+			return (NULL);
+		}
+		temp_buffer[bytes_read] = '\0';
+		new_buffer = ft_strjoin(buffer, temp_buffer);
+		free(buffer);
+		buffer = new_buffer;
+		if (ft_strchr(buffer, '\n') || bytes_read == 0)
+			break ;
+	}
+	free(temp_buffer);
+	return (buffer);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*holder = NULL;
+
+	char *line, *buffer, *temp;
+	line = NULL;
+	buffer = NULL;
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	temp = ft_update_holder(&holder);
+	buffer = ft_creatbuffer(fd, temp);
+	if (!buffer || !*buffer)
+	{
+		free(buffer);
+		return (NULL);
+	}
+	line = get_line(buffer);
+	if (ft_strchr(buffer, '\n'))
+		holder = ft_strdup(ft_strchr(buffer, '\n') + 1);
+	free(buffer);
+	return (line);
 }
--- getNextLine/get_next_line_utils.c
+++ getNextLine/get_next_line_utils.c
@@ -1,6 +1,5 @@
 #include "get_next_line.h"
 
-
 size_t	ft_strlen(const char *s)
 {
 	size_t	len;
@@ -11,29 +10,29 @@ size_t	ft_strlen(const char *s)
 	return (len);
 }
 
-char *ft_strjoin(char const *s1, char const *s2)
+char	*ft_strjoin(char const *s1, char const *s2)
 {
-    char *res;
-    size_t lens1;
-    size_t lens2;
+	char	*res;
+	size_t	lens1;
+	size_t	lens2;
 
-    lens1 = 0;
-    lens2 = 0;
-    if (!s1 && !s2)
-        return NULL;
-    if (s1)
-        lens1 = ft_strlen(s1);
-    if (s2)
-        lens2 = ft_strlen(s2);
-    res = malloc(lens1 + lens2 + 1);
-    if (!res)
-        return NULL;
-    if (s1)
-        ft_memcpy(res, s1, lens1);
-    if (s2)
-        ft_memcpy(res + lens1, s2, lens2);
-    res[lens1 + lens2] = '\0';
-    return res;
+	lens1 = 0;
+	lens2 = 0;
+	if (!s1 && !s2)
+		return (NULL);
+	if (s1)
+		lens1 = ft_strlen(s1);
+	if (s2)
+		lens2 = ft_strlen(s2);
+	res = malloc(lens1 + lens2 + 1);
+	if (!res)
+		return (NULL);
+	if (s1)
+		ft_memcpy(res, s1, lens1);
+	if (s2)
+		ft_memcpy(res + lens1, s2, lens2);
+	res[lens1 + lens2] = '\0';
+	return (res);
 }
 void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
@@ -43,8 +42,8 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 
 	if (!dst && !src)
 		return (NULL);
-    if (dst == src)
-        return (dst);
+	if (dst == src)
+		return (dst);
 	d = (unsigned char *)dst;
 	s = (unsigned char *)src;
 	i = 0;
